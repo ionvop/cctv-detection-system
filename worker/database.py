74e68fe23b449a7b167310687b58329d1777194c
ionvop <ionvop@gmail.com>
2026-03-05 19:34:51 +0800

    Create database.py

---
+++ worker/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+
+
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/_20260301"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    
+    try:
+        yield db
+    finally:
+        db.close()
