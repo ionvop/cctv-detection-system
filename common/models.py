48ea08268b7301b25e19eb848eca60d2e26cbd99
ionvop <ionvop@gmail.com>
2026-03-16 22:15:40 +0800

    Update models.py

--- common/models.py
+++ common/models.py
@@ -8,28 +8,37 @@ def unix_epoch():
     return int(time.time())
 
 
-class CCTV(Base):
-    __tablename__ = "cctvs"
+class Street(Base):
+    __tablename__ = "streets"
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False)
     time = Column(Integer, default=unix_epoch, nullable=False)
+
+    cctvs = relationship("CCTV", back_populates="street", cascade="all, delete")
+
+
+class CCTV(Base):
+    __tablename__ = "cctvs"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    street_id = Column(Integer, ForeignKey("streets.id", ondelete="CASCADE"), nullable=False)
+    name = Column(String(255), nullable=False)
+    time = Column(Integer, default=unix_epoch, nullable=False)
+
+    street = relationship("Street", back_populates="cctvs")
     detections = relationship("Detection", back_populates="cctv", cascade="all, delete")
 
 
 class Detection(Base):
     __tablename__ = "detections"
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     cctv_id = Column(Integer, ForeignKey("cctvs.id", ondelete="CASCADE"), nullable=False)
-    time = Column(Integer, default=unix_epoch, nullable=False)
-    cctv = relationship("CCTV", back_populates="detections")
-    coords = relationship("Coord", back_populates="detection", cascade="all, delete")
 
-
-class Coord(Base):
-    __tablename__ = "coords"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    detection_id = Column(Integer, ForeignKey("detections.id", ondelete="CASCADE"), nullable=False)
     x = Column(Float, nullable=False)
     y = Column(Float, nullable=False)
+
     time = Column(Integer, default=unix_epoch, nullable=False)
-    detection = relationship("Detection", back_populates="coords")
+
+    cctv = relationship("CCTV", back_populates="detections")
