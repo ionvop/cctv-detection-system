4c18a8c53aae399445dce36b224ccbe8b4076e57
ionvop <ionvop@gmail.com>
2026-03-30 01:02:50 +0800

    Update database.py

--- common/database.py
+++ common/database.py
@@ -11,6 +11,8 @@ engine = create_engine(
     max_overflow=20,
 )
 
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/_20260301"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
