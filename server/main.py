2ad082cf08485ef348daa288a603d681cedc4fe9
ionvop <ionvop@gmail.com>
2026-03-31 00:44:42 +0800

    Update main.py

--- server/main.py
+++ server/main.py
@@ -4,22 +4,13 @@ from datetime import datetime, timedelta, timezone
 from fastapi_utils.tasks import repeat_every
 from server.utils import SESSION_EXPIRATION
 from contextlib import asynccontextmanager
-from common.models import UserSession
 from fastapi import FastAPI
 from dotenv import load_dotenv
-
-
-@repeat_every(seconds=3600)
-async def delete_expired_sessions() -> None:
-    db = SessionLocal()
-    db.query(UserSession).filter(UserSession.time < datetime.now(timezone.utc) - timedelta(seconds=SESSION_EXPIRATION)).delete()
-    db.commit()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
-    await delete_expired_sessions()
     yield
 
 
