aed276c391c98124f85f1f55b58598e3f16e54af
ionvop <ionvop@gmail.com>
2026-03-30 12:15:35 +0800

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI
+from common.database import engine, Base, SessionLocal
+from datetime import datetime, timedelta, timezone
+from routers import user, login, intersection
+from fastapi_utils.tasks import repeat_every
+from server.utils import SESSION_EXPIRATION
 from contextlib import asynccontextmanager
-from common.database import engine, Base
-from .routers import cctv
-import common.models
+from common.models import UserSession
+from fastapi import FastAPI
+
+
+@repeat_every(seconds=3600)
+async def delete_expired_sessions() -> None:
+    db = SessionLocal()
+    db.query(UserSession).filter(UserSession.time < datetime.now(timezone.utc) - timedelta(seconds=SESSION_EXPIRATION)).delete()
+    db.commit()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
+    await delete_expired_sessions()
     yield
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(cctv.router)
+app.include_router(user.router)
+app.include_router(login.router)
+app.include_router(intersection.router)
