1767100fc1e3f590ea1bfecfea050821b4ed398e
ionvop <ionvop@gmail.com>
2026-03-30 14:58:22 +0800

    Update models.py

--- common/models.py
+++ common/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, UniqueConstraint
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
 from common.database import Base
+from sqlalchemy.sql import func
 
 
 class User(Base):
