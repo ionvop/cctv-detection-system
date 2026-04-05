27310486f17c8442615a308ec1f372947d3206fb
ionvop <ionvop@gmail.com>
2026-04-05 18:21:07 +0800

    Create detection.py

---
+++ server/routers/detection.py
@@ -0,0 +1,37 @@
+from server.schemas import DetectionResponse
+from server.utils import log_and_commit, get_current_user
+from fastapi import APIRouter, Depends, HTTPException
+from common.models import User, Intersection, CCTV, Detection
+from common.database import get_db
+from sqlalchemy.orm import Session
+from typing import Annotated, Optional
+from datetime import datetime
+
+
+router = APIRouter(
+    prefix="/detections",
+    tags=["Detections"]
+)
+
+
+@router.get("/cctv/{cctv_id}/", response_model=list[DetectionResponse])
+def get_detections(
+    cctv_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None
+) -> list[DetectionResponse]:
+    db_cctv = db.get(CCTV, cctv_id)
+
+    if not db_cctv:
+        raise HTTPException(status_code=404, detail="CCTV not found")
+    
+    db_detections = db.query(Detection).filter(Detection.cctv_id == cctv_id)
+
+    if start_time:
+        db_detections = db_detections.filter(Detection.time >= start_time)
+
+    if end_time:
+        db_detections = db_detections.filter(Detection.time <= end_time)
+
+    return db_detections.all()
